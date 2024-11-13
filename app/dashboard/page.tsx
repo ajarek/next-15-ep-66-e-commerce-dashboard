2e911eeea93e2d4a68e867f69495e114eadb60d8
Ajarek <ajarek@poczta.onet.pl>
2024-11-13 22:01:01 +0100

    added delete suidId

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,14 +10,15 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import Image from 'next/image'
-import { Button, buttonVariants } from '@/components/ui/button'
+import {  buttonVariants } from '@/components/ui/button'
 import Link from 'next/link'
 import { Plus } from 'lucide-react'
+import ButtonDelete from '@/components/ButtonDelete'
 
 const Dashboard = async () => {
   const suitsData = await fetchSuits()
   return (
-    <div className='flex flex-col items-center px-8'>
+    <div className='flex flex-col items-center px-8 max-sm:px-1'>
       <div className='flex items-center gap-8'>
       <h1 className='text-xl py-4 '>Lista Towarów</h1>
        <Link href='/add-suit' className={`${buttonVariants({variant: 'default', size:'icon'})} `}><Plus size={32} /></Link>
@@ -26,10 +27,10 @@ const Dashboard = async () => {
         <TableCaption>Koniec listy</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className=''>Foto</TableHead>
+            <TableHead className='max-sm:hidden'>Foto</TableHead>
             <TableHead className=''>Nazwa</TableHead>
-            <TableHead className=''>Opis</TableHead>
-            <TableHead className=''>Kolor</TableHead>
+            <TableHead className='max-sm:hidden'>Opis</TableHead>
+            <TableHead className='max-sm:hidden'>Kolor</TableHead>
             <TableHead className=''>Rozmiar</TableHead>
             <TableHead className=''>Cena</TableHead>
             <TableHead className=''>Akcja</TableHead>
@@ -38,7 +39,7 @@ const Dashboard = async () => {
         <TableBody>
           {suitsData.map((suit) => (
             <TableRow key={suit?.id}>
-              <TableCell className=''>
+              <TableCell className='max-sm:hidden'>
                 <Image
                   src={suit?.image || ''}
                   alt='foto'
@@ -47,14 +48,14 @@ const Dashboard = async () => {
                 />
               </TableCell>
               <TableCell className=''>{suit?.name}</TableCell>
-              <TableCell className=''>
+              <TableCell className='max-sm:hidden'>
                 {suit?.description.slice(0, 20)}...
               </TableCell>
-              <TableCell className=''>{suit?.color}</TableCell>
+              <TableCell className='max-sm:hidden'>{suit?.color}</TableCell>
               <TableCell className=''>{suit?.size}</TableCell>
               <TableCell className=''>{suit?.price.toFixed(2)}</TableCell>
               <TableCell className=''>
-                <Button size={'icon'}>❌</Button>
+                <ButtonDelete id={suit?.id as number} />
               </TableCell>
             </TableRow>
           ))}
---
+++ components/ButtonDelete.tsx
@@ -0,0 +1,35 @@
+'use client'
+import Form from 'next/form'
+import { Button } from '@/components/ui/button'
+import { deleteSuitId } from '@/lib/deleteSuitId'
+
+const ButtonDelete = ({id}: {id: number}) => {
+  
+  return (
+   
+      <Form
+       action={async (formData) => {
+        await deleteSuitId(formData)
+        
+      }}
+        className='w-full flex flex-col  items-start gap-4'
+      >
+        <input
+          type='hidden'
+          name='id'
+          value={id}
+        />
+
+        <Button
+          className=' bg-transparent  rounded-sm hover:bg-transparent hover:scale-105  transition'
+          type='submit'
+          size='sm'
+        >
+          ❌
+        </Button>
+      </Form>
+   
+  )
+}
+export default ButtonDelete
+
